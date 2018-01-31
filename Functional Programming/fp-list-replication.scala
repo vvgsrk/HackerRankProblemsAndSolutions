9d404104b347d719161018cc8cd2977039adc90d
vvgsrk <varanasi.rakeshkumar@gmail.com>
2018-01-31 15:58:06 +0400

    Scala Problem

---
+++ Functional Programming/fp-list-replication.scala
@@ -0,0 +1,21 @@
+Problem: https://www.hackerrank.com/challenges/fp-list-replication/problem
+
+Solution: def f(num:Int, arr:List[Int]): List[Int] = { 
+    
+    import scala.collection.mutable.ListBuffer
+    
+    var result_list = ListBuffer[Int]()
+    
+    for (i <- arr) { 
+      
+      for(j <- 1 to num) { 
+        
+        result_list += arr(i-1) 
+        }
+      
+    }
+    
+    result_list.toList
+  }
+  
+  
