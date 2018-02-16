7ae41b84cd65631b7bec990ccab0f244fa6eee9a
Varanasi Venkata G <varanasi.kumar@emirates.com>
2018-02-16 12:48:41 +0400

    operators

--- 30 Days of Code/operators.scala
+++ 30 Days of Code/operators.scala
@@ -1,6 +1,6 @@
-Problem: https://www.hackerrank.com/challenges/30-operators/problem
+/* Problem: https://www.hackerrank.com/challenges/30-operators/problem */
 
-Solution:
+/* Solution: */
 
 object Solution {
 
