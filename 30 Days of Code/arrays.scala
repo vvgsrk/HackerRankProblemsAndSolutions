5b6f0ffd60586928815a6b17897b7ccd0b49dfb2
vvgsrk <varanasi.rakeshkumar@gmail.com>
2018-02-26 21:33:35 +0400

    Arrays

---
+++ 30 Days of Code/arrays.scala
@@ -0,0 +1,16 @@
+/* Problem: https://www.hackerrank.com/challenges/30-arrays/problem */
+
+/* Solution: */
+
+object Solution {
+
+    def main(args: Array[String]) {
+        val sc = new java.util.Scanner (System.in);
+        var n = sc.nextInt();
+        var arr = new Array[Int](n);
+        for(arr_i <- 0 to n-1) {
+           arr(arr_i) = sc.nextInt();
+        }
+        println(arr.reverse.mkString(" "))
+    }
+}
